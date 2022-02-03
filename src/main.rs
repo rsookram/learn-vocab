94d644d319edabde8ac2e7702107189443afe7ca
rsookram <rashadsookram@gmail.com>
2022-02-02 19:58:32 -0500

    Load known words

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use std::env;
+use std::fs::File;
+use std::io;
+use std::io::BufRead;
 
 fn main() {
     let mut args = env::args();
@@ -7,5 +10,19 @@ fn main() {
     let file_path = args.next().unwrap();
     let db_path = args.next().unwrap();
 
-    println!("{}, {}", file_path, db_path);
+    let known_words = read_known_words(&file_path);
+
+    println!("{}, {:?}", db_path, known_words);
+}
+
+fn read_known_words(path: &str) -> Vec<String> {
+    let file = File::open(&path).unwrap();
+    let mut known_words = Vec::new();
+
+    let lines = io::BufReader::new(file).lines();
+    lines.flat_map(|line| line).for_each(|line| {
+        known_words.push(line);
+    });
+
+    known_words
 }
