4fb855886ac64adcc054b04ad588dcf04381291d
rsookram <rashadsookram@gmail.com>
2022-02-02 19:52:00 -0500

    Read command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    args.next(); // skip program name
+
+    let file_path = args.next().unwrap();
+    let db_path = args.next().unwrap();
+
+    println!("{}, {}", file_path, db_path);
 }
